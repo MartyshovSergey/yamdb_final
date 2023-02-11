2deaa75f12cc76677e3e8bb51105050c14e1e761
Сергей Мартышов <martyshovss@yandex.ru>
2023-02-11 20:24:43 +0400

    v1

---
+++ api_yamdb/reviews/models.py
@@ -0,0 +1,157 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+
+from api_yamdb.settings import REVIEW_CHARFIELD_LENGTH
+from user.models import CustomUser
+from .validators import year_valid
+
+
+class Category(models.Model):
+    name = models.CharField(
+        'Название категории',
+        max_length=REVIEW_CHARFIELD_LENGTH
+    )
+    slug = models.SlugField(
+        'slug категории',
+        unique=True,
+        db_index=True
+    )
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Comment(models.Model):
+    review = models.ForeignKey(
+        'Review',
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='отзыв'
+    )
+    text = models.CharField(
+        'текст комментария',
+        max_length=REVIEW_CHARFIELD_LENGTH
+    )
+    author = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='автор'
+    )
+    pub_date = models.DateTimeField(
+        'дата публикации',
+        auto_now_add=True,
+        db_index=True
+    )
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+
+    def __str__(self):
+        return self.text[0:30]
+
+
+class Genre(models.Model):
+    name = models.CharField(
+        'Название жанра',
+        max_length=REVIEW_CHARFIELD_LENGTH
+    )
+    slug = models.SlugField(
+        'slug жанра',
+        unique=True,
+        db_index=True
+    )
+
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    name = models.CharField(
+        'Название',
+        max_length=REVIEW_CHARFIELD_LENGTH,
+        db_index=True
+    )
+    year = models.IntegerField(
+        'Год',
+        validators=(year_valid,)
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        related_name='titles',
+        verbose_name='категория',
+        null=True,
+        blank=True
+    )
+    description = models.TextField(
+        'Описание',
+        max_length=255,
+        null=True,
+        blank=True
+    )
+    genre = models.ManyToManyField(
+        Genre,
+        related_name='titles',
+        verbose_name='жанр'
+    )
+
+    class Meta:
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
+
+    def __str__(self):
+        return self.name
+
+
+class Review(models.Model):
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='произведение'
+    )
+    text = models.CharField(
+        max_length=REVIEW_CHARFIELD_LENGTH
+    )
+    author = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='автор'
+    )
+    score = models.IntegerField(
+        'оценка',
+        validators=(
+            MinValueValidator(1),
+            MaxValueValidator(10)
+        ),
+        error_messages={'Проверка': 'Оценка в диапазоне от 1 до 10!'}
+    )
+    pub_date = models.DateTimeField(
+        'Дата публикации',
+        auto_now_add=True,
+        db_index=True
+    )
+
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
+        constraints = [
+            models.UniqueConstraint(
+                fields=('title', 'author',),
+                name='unique review'
+            )]
+        ordering = ('pub_date',)
+
+    def __str__(self):
+        return self.text[0:50]
